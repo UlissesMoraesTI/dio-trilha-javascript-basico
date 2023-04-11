2f8a80e4a006ad364338ae4e6d30341ab07494f8
Ulisses Moraes Dos Reis Souza <ulissesmoraes74@gmail.com>
2023-04-10 22:32:36 -0300

    Desafio - NUMERO PAR

---
+++ pares.js
@@ -0,0 +1,9 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (i = 0; i < numeros.length; i++) {
+
+    if (numeros[i] % 2 === 0) {
+        console.log('Numero ', numeros[i], ' é PAR.');
+    }
+
+}
